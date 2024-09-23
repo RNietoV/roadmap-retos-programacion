c10525a6d8c567773b0be860127cb957a0692041
velric <ricardo.nieto.vela@gmail.com>
2024-09-22 19:44:40 -0600

    mi primer commit 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RNietoV.py
@@ -0,0 +1,45 @@
+#
+# ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+# - Recuerda que todas las instrucciones de participación están en el
+#   repositorio de GitHub.
+#
+# Lo primero... ¿Ya has elegido un lenguaje?
+# - No todos son iguales, pero sus fundamentos suelen ser comunes.
+# - Este primer reto te servirá para familiarizarte con la forma de participar
+#   enviando tus propias soluciones.
+#
+# EJERCICIO:
+# - Crea un comentario en el código y coloca la URL del sitio web oficial del
+#   lenguaje de programación que has seleccionado.
+# - Representa las diferentes sintaxis que existen de crear comentarios
+#   en el lenguaje (en una línea, varias...).
+# - Crea una variable (y una constante si el lenguaje lo soporta).
+# - Crea variables representando todos los tipos de datos primitivos
+#   del lenguaje (cadenas de texto, enteros, booleanos...).
+# - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+#
+# ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+# debemos comenzar por el principio.
+
+
+# https://www.python.org/
+
+# se usa la almoadilla '#' se cre una linea de ccomentario 
+
+"""
+Esto tambien es un comentario
+"""
+'''
+esto tambien es un comentario
+'''
+
+
+my_variable = 'Mi variable'
+MY_CONSTAN = 'POR CONVENCION ESTO ES UNA CONSTANTE, PORFA NO LA CAMBIES'
+
+my_string = 'Hello word'
+my_integer = 7
+my_bool = True
+my_float = 1.1
+
+print('Hola soy Ricardo y mi leguaje es Python')
